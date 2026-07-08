26a5d6bb95901d009dd64207eb975810b57ac2e1
aiutilities <cogzidel2k@gmail.com>
2026-07-08 16:53:11 +0530

    feat(search): register property search provider

--- backend/src/core/property/property.module.ts
+++ backend/src/core/property/property.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { PostgresModule } from '../../database/postgres/postgres.module';
 import { IdentityModule } from '../identity/identity.module';
+import { SearchModule } from '../search';
 
 import { PropertyController } from './controllers/property.controller';
 
@@ -10,12 +11,14 @@ import {
 } from './services/property.service';
 
 import { PostgresPropertyRepository } from './repositories/postgres-property.repository';
+import { PropertySearchProviderService } from './property-search-provider.service';
 
 @Module({
-  imports: [PostgresModule, IdentityModule],
+  imports: [PostgresModule, IdentityModule, SearchModule],
   controllers: [PropertyController],
   providers: [
     PropertyService,
+    PropertySearchProviderService,
     {
       provide: PROPERTY_REPOSITORY,
       useClass: PostgresPropertyRepository,

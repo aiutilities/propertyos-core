75cd58d4cfb9b38100fa9f2e04ea4e6eeb7ca454
aiutilities <cogzidel2k@gmail.com>
2026-07-08 17:37:13 +0530

    feat(search): register tenant search provider

--- backend/src/core/tenant/tenant.module.ts
+++ backend/src/core/tenant/tenant.module.ts
@@ -3,16 +3,19 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../../database/database.module';
 import { IdentityModule } from '../identity/identity.module';
 import { EventBusModule } from '../eventbus/eventbus.module';
+import { SearchModule } from '../search';
 import { TenantController } from './controllers/tenant.controller';
 import { TENANT_REPOSITORY } from './repositories/tenant-repository.interface';
 import { PostgresTenantRepository } from './repositories/postgres-tenant.repository';
 import { TenantService } from './services/tenant.service';
+import { TenantSearchProviderService } from './tenant-search-provider.service';
 
 @Module({
-  imports: [DatabaseModule, IdentityModule, EventBusModule],
+  imports: [DatabaseModule, IdentityModule, EventBusModule, SearchModule],
   controllers: [TenantController],
   providers: [
     TenantService,
+    TenantSearchProviderService,
     {
       provide: TENANT_REPOSITORY,
       useClass: PostgresTenantRepository,
